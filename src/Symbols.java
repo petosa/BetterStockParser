552d892f32ac068f2b70a748fe9638f0f45c8e49
Nick Peto <nick.petosa@gmail.com>
2016-04-28 17:32:02 -0400

    Removed jaunt in favor of CSV parsing

--- src/Symbols.java
+++ src/Symbols.java
@@ -2,7 +2,13 @@ import com.jaunt.*;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
+import com.opencsv.CSVReader;
 import org.bson.Document;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,13 +38,13 @@ public class Symbols {
     /**
      * Updates Mongo's list of symbols from internet.
      */
-    public static void update() throws NotFound, ResponseException {
+    public static void update() throws NotFound, ResponseException, IOException {
         db.getCollection("symbols").drop();
         db.createCollection("symbols");
-        List<String> arr = scrapeSymbols();
+        List<Document> arr = scrapeSymbols();
         ArrayList<Document> docs = new ArrayList<>();
-        for (String s : arr) {
-            docs.add(new Document("symbol", s));
+        for (Document d : arr) {
+            docs.add(d);
         }
         db.getCollection("symbols").insertMany(docs);
         System.out.println("Updated symbols (" + db.getCollection("symbols").count() + " total)");
@@ -49,15 +55,25 @@ public class Symbols {
      *
      * @return a list of scraped symbols.
      */
-    private static List<String> scrapeSymbols() throws ResponseException, NotFound {
-        UserAgent ua = new UserAgent();
-        ua.visit("https://www.cboe.com/tradtool/symbols/symbolweeklys.aspx");
-        Elements rows = ua.doc.findEvery("<td align=\"Center\">");
-        ArrayList<String> arr = new ArrayList<>();
-        for (Element i : rows) {
-            arr.add(i.innerText());
+    private static List<Document> scrapeSymbols() throws IOException {
+        URL url = new URL("https://www.cboe.com/publish/scheduledtask/mktdata/cboesymboldirweeklys.csv");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(url.openStream()));
+        CSVReader csvReader = new CSVReader(in);
+        String[] row = null;
+        ArrayList<Document> arr = new ArrayList<>();
+        boolean first = true;
+        while((row = csvReader.readNext()) != null) {
+            if(!first) {
+                Document d = new Document("name", row[0])
+                        .append("symbol", row[1]);
+                arr.add(d);
+            } else {
+                first = false;
+            }
         }
         arr.remove(0);
+        csvReader.close();
         return arr;
     }
 
